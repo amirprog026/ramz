26536fbfe9e67d651a77944f8bf0612543fc41dc
amirprog026 <amirdev2024@gmail.com>
2024-11-09 11:10:34 +0330

    Update forms.py

--- forms.py
+++ forms.py
@@ -5,3 +5,6 @@ from wtforms.validators import DataRequired
 class EncryptForm(FlaskForm):
     text = StringField('Text to Encrypt', validators=[DataRequired()])
     submit = SubmitField('Encrypt')
+class DecryptForm(FlaskForm):
+    encrypted_text = StringField('Text to Decrypt', validators=[DataRequired()])
+    submit = SubmitField('Decrypt')
